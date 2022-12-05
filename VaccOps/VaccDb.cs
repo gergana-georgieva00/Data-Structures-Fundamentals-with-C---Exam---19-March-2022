cc7c65d5649cbfd8f2525de8b0ee5d2e50345d3b
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-06 00:20:09 +0200

    Add project files.

---
+++ VaccOps/VaccDb.cs
@@ -0,0 +1,84 @@
+﻿namespace VaccOps
+{
+    using Models;
+    using Interfaces;
+    using System;
+    using System.Collections.Generic;
+
+    public class VaccDb : IVaccOps
+    {
+        private List<Doctor> doctors;
+        private List<Patient> patients;
+
+        public VaccDb()
+        {
+            doctors = new List<Doctor>();
+            patients = new List<Patient>();
+        }
+
+        public void AddDoctor(Doctor doctor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPatient(Doctor doctor, Patient patient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeDoctor(Doctor oldDoctor, Doctor newDoctor, Patient patient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exist(Doctor doctor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exist(Patient patient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Doctor> GetDoctors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Doctor> GetDoctorsByPopularity(int populariry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Doctor> GetDoctorsSortedByPatientsCountDescAndNameAsc()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Patient> GetPatients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Patient> GetPatientsByTown(string town)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Patient> GetPatientsInAgeRange(int lo, int hi)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Patient> GetPatientsSortedByDoctorsPopularityAscThenByHeightDescThenByAge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Doctor RemoveDoctor(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
